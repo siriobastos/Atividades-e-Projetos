3f2601d38b8988a6339c6640ac46f29ee517d483
siriobastos <siriobastos@gmail.com>
2024-01-16 17:04:55 -0300

    arquivos em python hotel crescente e decrescente

---
+++ # Ordem Crescente.py
@@ -0,0 +1,11 @@
+# Usando o loop for
+for andar in range(1, 21):
+    if andar != 13:
+        print(andar)
+
+# Usando o loop while
+andar = 1
+while andar <= 20:
+    if andar != 13:
+        print(andar)
+    andar += 1
